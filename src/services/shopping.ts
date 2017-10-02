27faf6b97d7f4cfdde1e70e87e6e9087d1f487d3
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-02 19:27:34 +0200

    creating Shopping Services

--- src/services/shopping.ts
+++ src/services/shopping.ts
@@ -1,3 +1,21 @@
-export class ShoppingService {
+import {Ingredient} from "../models/ingridient";
 
+export class ShoppingService {
+  private ingredients: Ingredient[] = [];
+
+  addItem(name: string, amount: number){
+    this.ingredients.push(new Ingredient(name, amount))
+  }
+
+  addMultipleItem(items: Ingredient[]){
+    this.ingredients.push(...items)
+  }
+
+  getItem(){
+    return this.ingredients.slice();
+  }
+
+  removeItem(index: number){
+    return this.ingredients.splice(index, 1);
+  }
 }
