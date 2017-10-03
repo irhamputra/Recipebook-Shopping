9ab9fe49afaeaed4b0309cea4cc69912c20f7398
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-03 15:23:29 +0200

    submit handler, creating manage recipe

--- src/pages/edit-recipes/edit-recipes.ts
+++ src/pages/edit-recipes/edit-recipes.ts
@@ -1,30 +1,56 @@
-import {Component, OnInit} from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component, OnInit} from "@angular/core";
+import {NavController, NavParams, ActionSheetController} from "ionic-angular";
 import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'page-edit-recipes',
   templateUrl: 'edit-recipes.html',
 })
-export class EditRecipesPage implements OnInit{
+export class EditRecipesPage implements OnInit {
 
   // Reactive Form
   mode = "New";
   selectOptions = ["Easy", "Medium", "Hard"];
   recipeForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public actionSheetCtrl: ActionSheetController) {
   }
 
-  ngOnInit(){
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  addManageRecipe() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Add Ingredients',
+      buttons: [{
+        text: 'Add Ingredients',
+        handler: () => {
+
+        }
+      },
+        {
+          text: 'Remove all',
+          role: 'destructive',
+          handler: () => {
+
+          }
+        }]
+    })
+  }
+
+  ngOnInit() {
     this.mode = this.navParams.get('mode');
+    this.initForm()
   }
 
-  private initForm(){
+  private initForm() {
     this.recipeForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
-      'description' : new FormControl(null, Validators.required),
-      'difficulty': new FormControl('Medium', Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Easy', Validators.required),
 
     })
   }
