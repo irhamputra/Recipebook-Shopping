42c9dd61190afcf1c0c24f19a64202aa15a02f5b
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-03 19:49:08 +0200

    Outputting value in a list

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { EditRecipesPage } from "../edit-recipes/edit-recipes";
+import {Recipe} from "../../models/recipe";
+import {RecipeServices} from "../../services/recipe";
 
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private recipes: Recipe[] = [];
+
+  constructor(public navCtrl: NavController,
+              public recipeService: RecipeServices) {
+  }
+
+  ionViewWillEnter(){
+    this.recipes = this.recipeService.getRecipe()
   }
 
   toAddRecipe(){
     this.navCtrl.push(EditRecipesPage, { mode: 'New' })
   }
 
+  onLoadRecipe(){
+
+  }
 }
