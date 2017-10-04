0ac343c956fc1c21e3b0e9780c7a2e06314ebe29
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-04 10:59:09 +0200

    sign in service and implement service

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "../../services/auth";
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
 })
 export class SigninPage {
+
+  constructor(private authService: AuthService){}
+
   signInUser(form: NgForm){
-    console.log(form.value);
+    let formEmail = form.value.email;
+    let formPassword = form.value.password;
+    this.authService.signIn(formEmail, formPassword)
+      .then(data => { console.log(data) })
+      .catch(error => { console.log(error) });
   }
 }
